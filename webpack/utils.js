34a6cd2c15e7e87ace751d0794b4791ebf074fd5
Colton Wrisner <colton.d.wrisner@saic.com>
2020-10-28 13:02:51 -0500

    pre-commit fixes

--- webpack/utils.js
+++ webpack/utils.js
@@ -4,7 +4,7 @@ const tsconfig = require('../tsconfig.base.json');
 
 module.exports = {
   root,
-  mapTypescriptAliasToWebpackAlias
+  mapTypescriptAliasToWebpackAlias,
 };
 
 const _root = path.resolve(__dirname, '..');
