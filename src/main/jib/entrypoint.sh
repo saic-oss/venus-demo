28da0f4438a62f39ff5960fcd3b84b634aeee20a
Rachel Ania <rakjennings@gmail.com>
2021-08-02 13:46:10 -0500

    venusapp name change (#7)
    Co-authored-by: Ania Jennings <rachel.a.kaczka.jennings@saic.com>

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -4,4 +4,4 @@ echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep "${JHIPSTER_
 
 # Wrapping ${JAVA_OPTS} in quotes breaks the image such that it won't start properly
 # shellcheck disable=SC2086
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.saic.demos.venusdemo.VenusdemoApp"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.saic.demos.VenusApp"  "$@"
--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -1,7 +1,7 @@
 import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ProdConfig } from './blocks/config/prod.config';
-import { VenusdemoAppModule } from './app.module';
+import { VenusAppModule } from './app.module';
 
 ProdConfig();
 
@@ -10,7 +10,7 @@ if (module['hot']) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(VenusdemoAppModule, { preserveWhitespaces: true })
+  .bootstrapModule(VenusAppModule, { preserveWhitespaces: true })
   // eslint-disable-next-line no-console
   .then(() => console.log('Application started'))
   .catch(err => console.error(err));
